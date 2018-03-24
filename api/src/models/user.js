dc76ab05731e5b567a35455ee53d0f9d9e2d6f17
Zeljko <zeljko.baranek@gmail.com>
2018-03-24 12:15:30 +0100

    Update mongoose user schema
    - username unique,
    -username and password required,
    - default value for createdAt set to Date.now.

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -5,9 +5,20 @@ const mongoose = require('mongoose');
 
 // User Collection
 let UserSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    createdAt: Date
+    username: {
+        type: String,
+        index: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 let User = mongoose.model('users', UserSchema);
