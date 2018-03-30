8efdc6711ec96a810ddd655a8962f04f91a62126
Zeljko <zeljko.baranek@gmail.com>
2018-03-30 23:59:52 +0200

    Add production and development .json files for frontend

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,14 +1,4 @@
 // Configurations for Frontend
 const NODE_ENV = (process.env.NODE_ENV === 'production') ? 'production' : 'development';
 
-let config = {
-    url: {}
-};
-
-if(NODE_ENV === 'production') {
-    config.url.api = 'https://www.memoapp.net/api/'; // Change this URL according to your live server
-} else {
-    config.url.api = '/api/'
-}
-
-export default config;
+export default require(`./${NODE_ENV}.json`);
