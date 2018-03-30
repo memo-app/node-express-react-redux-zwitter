4962e5f3b8eb608be1b9973f35eb4c0e8212735f
Zeljko <zeljko.baranek@gmail.com>
2018-03-30 19:35:58 +0200

    Add development and production config JSON

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,11 +1,5 @@
 // src / config / index.js
 'use strict';
 
-const config = {
-    port: 5001,
-    secret: 'super-secret-key',
-    databaseUrl: 'mongodb://localhost/memoapp',
-    saltRounds: 10
-};
-
-module.exports = config;
+const env = process.env.NODE_ENV || 'development';
+module.exports = require(`${__dirname}/${env}.json`);
